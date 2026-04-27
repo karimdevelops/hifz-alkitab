84f98f4bd1837fac371fc878060cf2809013eff1
Abdul Karim <karim.develops@gmail.com>
2026-04-27 19:47:32 +0500

    refactor: replace hardcoded api with env

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,7 +2,12 @@ import { Chapter } from "@/app/lib/definitions";
 
 export async function fetchChapters(): Promise<Chapter[]> {
   try {
-    const allChaps = await fetch("https://quranapi.pages.dev/api/surah.json");
+    const { CHAPTERS_API_URL } = process.env;
+
+    if (!CHAPTERS_API_URL)
+      throw new Error("Missing CHAPTERS_API_URL key in env variables.");
+
+    const allChaps = await fetch(CHAPTERS_API_URL);
     const chapsData = await allChaps.json();
     return chapsData;
   } catch (e) {
