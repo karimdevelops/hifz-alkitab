d399ba41fb313759215303fd804d41614809f1db
Abdul Karim <karim.develops@gmail.com>
2026-04-28 18:23:18 +0500

    style: increase skeleton roundness

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="flex flex-col gap-5">
-      <Skeleton className="size-20 w-full" />
-      <Skeleton className="size-20 w-full" />
-      <Skeleton className="size-20 w-full" />
-      <Skeleton className="size-20 w-full" />
-      <Skeleton className="size-20 w-full" />
-      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full rounded-xl" />
+      <Skeleton className="size-20 w-full rounded-xl" />
+      <Skeleton className="size-20 w-full rounded-xl" />
+      <Skeleton className="size-20 w-full rounded-xl" />
+      <Skeleton className="size-20 w-full rounded-xl" />
+      <Skeleton className="size-20 w-full rounded-xl" />
     </div>
   );
 }
