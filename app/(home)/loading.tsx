dfe2d93532928de24598619003aa806431619d86
Abdul Karim <karim.develops@gmail.com>
2026-04-28 16:51:47 +0500

    style: increase skeleton loading size

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -2,13 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-10">
-      <Skeleton className="size-16 w-full" />
-      <Skeleton className="size-16 w-full" />
-      <Skeleton className="size-16 w-full" />
-      <Skeleton className="size-16 w-full" />
-      <Skeleton className="size-16 w-full" />
-      <Skeleton className="size-16 w-full" />
+    <div className="flex flex-col gap-5">
+      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full" />
+      <Skeleton className="size-20 w-full" />
     </div>
   );
 }
