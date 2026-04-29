3fcaf5f4d0799781bcd9a2c30d8fdaae61357fa7
Abdul Karim <karim.develops@gmail.com>
2026-04-29 09:09:05 +0500

    refactor: isolate client component

---
+++ app/(home)/_components/ChapterTab.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function ChapterTab() {
+  const pathname = usePathname();
+
+  return (
+    <ul className="flex max-w-fit justify-center gap-2 self-center font-semibold *:rounded-full *:px-3 *:py-2">
+      <li
+        className={`${pathname == "/surah" ? "bg-app-primary text-app-background" : ""}`}
+      >
+        <Link href={"/surah"}>Surah</Link>
+      </li>
+      <li
+        className={`${pathname == "/reveal-order" ? "bg-app-primary text-app-background" : ""}`}
+      >
+        <Link href={"/reveal-order"}>Revelation Order</Link>
+      </li>
+    </ul>
+  );
+}
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,33 +1,20 @@
-"use client";
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import ChapterTab from "@/app/(home)/_components/ChapterTab";
+import { Suspense } from "react";
 
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
   return (
-    <div className="flex min-h-[90vh] flex-col gap-10 p-5 md:p-10">
-      <h1 className="text-app-primary font-noto-arabic self-center text-4xl font-bold">
+    <div className="flex min-h-[90vh] flex-col gap-5 px-5 py-10 md:p-10">
+      <h1 className="text-app-primary font-kitab self-center text-5xl font-bold">
         حفظ
       </h1>
-      <ul className="flex max-w-fit justify-center gap-2 self-center font-semibold *:rounded-full *:px-3 *:py-2">
-        <li
-          className={`${pathname == "/surah" ? "bg-app-primary text-app-background" : ""}`}
-        >
-          <Link href={"/surah"}>Surah</Link>
-        </li>
-        <li
-          className={`${pathname == "/reveal-order" ? "bg-app-primary text-app-background" : ""}`}
-        >
-          <Link href={"/reveal-order"}>Revelation Order</Link>
-        </li>
-      </ul>
-      <div>{children}</div>
+      <Suspense>
+        <ChapterTab />
+        <div>{children}</div>
+      </Suspense>
     </div>
   );
 }
