7756e818799fea1edb51ba07012e566a531c311e
Abdul Karim <karim.develops@gmail.com>
2026-04-30 08:56:52 +0500

    feat: add ayah and surah interfaces

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -3,6 +3,21 @@ export interface Chapter {
   name: string;
   englishName: string;
   englishNameTranslation: string;
-  revelationPlace: string;
+  revelationType: string;
   numberOfAyahs: number;
+}
+
+export interface Ayah {
+  numberInSurah: number;
+  text: string;
+}
+
+export interface Surah {
+  number: number;
+  name: string;
+  englishName: string;
+  englishNameTranslation: string;
+  revelationType: string;
+  numberOfAyahs: number;
+  ayahs: Ayah[];
 }
