1a392dbb8241e4fd3484c4d0a4e7fd26083f1f66
Abdul Karim <karim.develops@gmail.com>
2026-05-23 05:16:18 +0500

    feat: tap logic for mobile devices

---
+++ components/Word.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { motion } from "motion/react";
+import { useState } from "react";
+
+export default function Word({ text, i }: { text: string; i: number }) {
+  const [isHoverTap, setHoverTap] = useState(false);
+
+  function setHoverTapFalse() {
+    setTimeout(() => {
+      setHoverTap(false);
+    }, 2000);
+  }
+
+  return (
+    <motion.span
+      animate={
+        isHoverTap
+          ? {
+              color: "var(--app-foreground)",
+              translateY: -5,
+              borderColor: "var(--app-primary)",
+            }
+          : {}
+      }
+      onMouseEnter={() => setHoverTap(true)}
+      onTapStart={() => setHoverTap(true)}
+      onTap={setHoverTapFalse}
+      onMouseLeave={setHoverTapFalse}
+      key={i}
+      className="border-app-foreground inline-block cursor-pointer border-b text-transparent"
+    >
+      {text}
+    </motion.span>
+  );
+}
