728806d9c8f536610585d1fd9c73bf7725cbecb4
Abdul Karim <karim.develops@gmail.com>
2026-05-23 13:07:08 +0500

    chore: replace nav settings pos with title

--- app/[order]/[surah]/layout.tsx
+++ app/[order]/[surah]/layout.tsx
@@ -8,13 +8,13 @@ export default async function SurahLayout({
   return (
     <div className="flex min-h-[90vh] flex-col gap-10 px-5 py-10 md:p-10">
       <div className="flex justify-between">
+        <div>settings</div>
         <Link
           href="/"
           className="text-app-primary font-kitab text-3xl font-bold"
         >
           حفظ
         </Link>
-        <div>settings</div>
       </div>
       <div>{children}</div>
     </div>
