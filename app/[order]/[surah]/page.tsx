be2d171b0e14499d180edb33af4c45803346dcf2
Abdul Karim <karim.develops@gmail.com>
2026-05-24 10:37:32 +0500

    style: gap between surah name and ayah

--- app/[order]/[surah]/page.tsx
+++ app/[order]/[surah]/page.tsx
@@ -20,26 +20,28 @@ export default async function Home({
       <div
         dir="rtl"
         lang="ar"
-        className="font-indopak max-w-165 px-1 text-center"
+        className="font-indopak flex max-w-165 flex-col gap-10 px-1 text-center"
       >
         <div className="font-surah-name text-app-primary bg-app-primary-darker border-app-primary min-w-max rounded-xl border py-1 text-center text-5xl">
           {"surah" + fNum}
         </div>
-        {surahData != null
-          ? surahData.ayahs.map((ayah, i: number) => (
-              <span
-                className="gap-2 *:ml-2 *:text-3xl/20 *:md:text-4xl/20"
-                key={i}
-              >
-                {ayah.text.split(" ").map((word, i) => (
-                  <Word text={word} key={i} />
-                ))}
-                <span className="font-digital-khatt px-2 align-middle">
-                  {"\u06DD" + ayah.numberInSurah.toLocaleString("ar-EG")}
+        <div>
+          {surahData != null
+            ? surahData.ayahs.map((ayah, i: number) => (
+                <span
+                  className="gap-2 *:ml-2 *:text-3xl/20 *:md:text-4xl/20"
+                  key={i}
+                >
+                  {ayah.text.split(" ").map((word, i) => (
+                    <Word text={word} key={i} />
+                  ))}
+                  <span className="font-digital-khatt px-2 align-middle">
+                    {"\u06DD" + ayah.numberInSurah.toLocaleString("ar-EG")}
+                  </span>
                 </span>
-              </span>
-            ))
-          : null}
+              ))
+            : null}
+        </div>
       </div>
     </div>
   );
