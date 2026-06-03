b38f371938bd5efbe014ba365a33bfa27d400fd0
Abdul Karim <karim.develops@gmail.com>
2026-06-03 06:11:16 +0500

    feat: fetch data for local prod based on env

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,34 +1,34 @@
 import { Chapter, ChaptersResponse, Surah } from "@/app/lib/definitions";
 
+const LOCAL_DEV = process.env.LOCAL_DEV === "true";
+
+const CHAPTERS_API_URL = LOCAL_DEV
+  ? process.env.CHAPTERS_API_URL_LOCAL!
+  : process.env.CHAPTERS_API_URL_PROD!;
+
+const SURAH_API_URL = LOCAL_DEV
+  ? process.env.SURAH_API_URL_LOCAL!
+  : process.env.SURAH_API_URL_PROD!;
+
 export async function fetchChapters(reveal: boolean): Promise<Chapter[]> {
   try {
-    let allChaps;
-    const { CHAPTERS_API_URL } = process.env;
+    const res = await fetch(CHAPTERS_API_URL, { next: { revalidate: false } });
+    const data: ChaptersResponse = await res.json();
 
-    if (!CHAPTERS_API_URL)
-      throw new Error("Missing ...API_URL key in env variables.");
-    else
-      allChaps = await fetch(CHAPTERS_API_URL, {
-        next: { revalidate: false },
-      });
-
-    const chapsData: ChaptersResponse = await allChaps.json();
-    const fChapsData = chapsData.chapters.map((chap: Chapter) => {
-      return {
-        id: chap.id,
-        revelation_order: chap.revelation_order,
-        bismillah_pre: chap.bismillah_pre,
-        name_simple: chap.name_simple,
-        name_arabic: chap.name_arabic,
-        verses_count: chap.verses_count,
-        translated_name: chap.translated_name,
-      };
-    });
+    let chapters = data.chapters.map((chap) => ({
+      id: chap.id,
+      revelation_order: chap.revelation_order,
+      bismillah_pre: chap.bismillah_pre,
+      name_simple: chap.name_simple,
+      name_arabic: chap.name_arabic,
+      verses_count: chap.verses_count,
+      translated_name: chap.translated_name,
+    }));
 
     if (reveal)
-      fChapsData.sort((a, b) => a.revelation_order - b.revelation_order);
+      chapters.sort((a, b) => a.revelation_order - b.revelation_order);
 
-    return fChapsData;
+    return chapters;
   } catch (e) {
     console.log("API error", e);
     return [];
@@ -37,24 +37,20 @@ export async function fetchChapters(reveal: boolean): Promise<Chapter[]> {
 
 export async function fetchSurah(number: number): Promise<Surah | null> {
   try {
-    const { SURAH_API_URL, CHAPTERS_API_URL } = process.env;
-
-    if (!CHAPTERS_API_URL || !SURAH_API_URL)
-      throw new Error("Missing API_URL in env variables.");
+    const verseUrl = LOCAL_DEV
+      ? `${SURAH_API_URL}/${number}`
+      : `${SURAH_API_URL}${number}?language=ar&words=true&word_fields=text_indopak&per_page=all`;
 
     const [metaRes, verseRes] = await Promise.all([
       fetch(`${CHAPTERS_API_URL}/${number}`, { next: { revalidate: false } }),
-      fetch(
-        // `${SURAH_API_URL}${number}?language=ar&words=true&word_fields=text_indopak&per_page=all`,
-        `${SURAH_API_URL}/${number}`,
-        { next: { revalidate: false } },
-      ),
+      fetch(verseUrl, { next: { revalidate: false } }),
     ]);
+
     const metaData = await metaRes.json();
     const verseData = await verseRes.json();
 
     return {
-      ...metaData,
+      ...(LOCAL_DEV ? metaData : metaData.chapter),
       verses: verseData.verses,
     };
   } catch (e) {
