367e6550bca8356b1667bb2f692937b7b4d2abf4
Abdul Karim <karim.develops@gmail.com>
2026-06-09 20:01:43 +0500

    feat: create auth api route

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,21 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
-
+function poolConfig() {
+  if (process.env.PROD == "true") {
+    return new Pool({
+      connectionString: process.env.DB_STRING,
+    });
+  } else {
+    return new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB,
+    });
+  }
+}
 export const auth = betterAuth({
-  database: new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-  }),
+  database: poolConfig(),
   emailAndPassword: {
     enabled: true,
   },
